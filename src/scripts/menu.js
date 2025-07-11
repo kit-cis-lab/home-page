1b2b208e313288ab9b82f85dae6b0383b28e8e62
shunsei <k.shunsei1227@gmail.com>
2025-07-11 22:39:02 +0900

    refactor menu.js

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,6 @@
-document.querySelector('.hamburger').addEventListener('click', () => {
-  document.querySelector('.nav-links').classList.toggle('expanded');
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('expanded');
 });
