5b3d524ab4708fd9a9378611ff2ec02549c3f17e
shunsei <k.shunsei1227@gmail.com>
2025-07-12 16:18:14 +0900

    refactor navigation component

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,6 +1,28 @@
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
+const hamburger = document.getElementById('hamburger');
+const overlay = document.getElementById('overlay');
+const drawer = document.getElementById('drawer');
+const drawerCloseButton = document.getElementById('drawerCloseButton');
 
-hamburger.addEventListener('click', () => {
-  navLinks.classList.toggle('expanded');
-});
+function openDrawer() {
+  overlay.classList.remove('hidden');
+  drawer.classList.remove('hidden');
+  
+  setTimeout(() => {
+    drawer.classList.remove('translate-x-full');
+    drawer.classList.add('translate-x-0');
+  }, 10); // overlay と drawer が表示されるのを待つ
+}
+
+function closeDrawer() {
+  drawer.classList.remove('translate-x-0');
+  drawer.classList.add('translate-x-full');
+
+  setTimeout(() => {
+    overlay.classList.add('hidden');
+    drawer.classList.add('hidden');
+  }, 300); // drawer が完全に閉じるのを待つ
+}
+
+hamburger.addEventListener('click', openDrawer);
+overlay.addEventListener('click', closeDrawer);
+drawerCloseButton.addEventListener('click', closeDrawer);
