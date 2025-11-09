736db15d8b73e34e2ca5038afc55ccd61701b8c2
popupie <107357501+phakpoomachalanan@users.noreply.github.com>
2025-11-09 16:57:08 +0900

    define achievement schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,12 +1,25 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const newsCollection = defineCollection({
-  type: 'content',
+  type: "content",
   schema: z.object({
     date: z.date(),
   }),
 });
 
+const achievementsCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    date: z.date(),
+    title: z.string(),
+    tags: z.array(z.string()),
+    authors: z.array(z.string()),
+    year: z.string(),
+    publisher: z.string(),
+  }),
+});
+
 export const collections = {
   news: newsCollection,
+  achievements: achievementsCollection,
 };
